bdb15b26f7dcadfc1106a179baac82c60d09227c
Irina <zaletk.irina@gmail.com>
2019-11-09 11:43:01 +0300

    feat : add  state and function getSizes

--- palette/src/assets/js/js.js
+++ palette/src/assets/js/js.js
@@ -1,24 +1,40 @@
 import size4x4 from './size4x4';
-import size32x32 from './size32x32';
-import rslogo from '../img/rs.png';
 import '../main.css';
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const imgRs = new Image();
-imgRs.src = rslogo;
+const state = {
+  tools: {
+    pencil: true,
+    bucket: false,
+    picker: false,
+  },
+  colors: {
+    current: '#c4c4c4',
+    prev: '#41f795',
+  },
+  canvasSize: {
+    width: 0,
+    height: 0,
+  },
+  blockSize: {
+    width: 0,
+    height: 0,
+  },
+  matrix: size4x4,
+};
 
-function draw(array, key) {
-  const scale = key === 'size32x32' ? 17 : 130;
-  array.forEach((row, indx1) => row.forEach((col, indx2) => {
-    ctx.fillStyle = Array.isArray(col) ? `rgba(${col[0]},${col[1]},${col[2]},${col[3]}` : `#${col}`;
-    ctx.fillRect(indx2 * scale, indx1 * scale, scale, scale);
-  }));
-}
-
-const drawImg = () => ctx.drawImage(imgRs, 0, 0, canvas.width, canvas.height);
+const getSizes = (cloth, array, obj) => {
+  const states = obj;
+  states.canvasSize.width = cloth.width;
+  states.canvasSize.height = cloth.height;
+  states.blockSize.width = cloth.width / array.length;
+  states.blockSize.height = cloth.height / array[0].length;
+};
 
 
-document.querySelector('.draw4x4').addEventListener('click', () => draw(size4x4, 'size4x4'));
-document.querySelector('.draw32x32').addEventListener('click', () => draw(size32x32, 'size32x32'));
-document.querySelector('.drawRschool').addEventListener('click', drawImg);
+const init = () => {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  getSizes(canvas, size4x4, state);
+};
+
+init();
