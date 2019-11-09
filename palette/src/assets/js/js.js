8a976daf011dfa28eef922e5ce8b9cc29f1fe20a
Irina <zaletk.irina@gmail.com>
2019-11-09 11:45:55 +0300

    feat: add  function  drawCanvas

--- palette/src/assets/js/js.js
+++ palette/src/assets/js/js.js
@@ -1,3 +1,4 @@
+import { isArray } from 'util';
 import size4x4 from './size4x4';
 import '../main.css';
 
@@ -30,11 +31,26 @@ const getSizes = (cloth, array, obj) => {
   states.blockSize.height = cloth.height / array[0].length;
 };
 
+const drawCanvas = (array, canvasItem, ctxItem) => {
+  const scale = canvasItem.width / array.length;
+  const context = ctxItem;
+  array.forEach((row, rowIndex) => {
+    row.forEach((col, colIndex) => {
+      if (isArray(col)) {
+        context.fillStyle = `rgba(${col[0]}, ${col[1]}, ${col[2]}, ${col[3]})`;
+      } else {
+        context.fillStyle = `#${col}`;
+      }
+      context.fillRect(colIndex * scale, rowIndex * scale, scale, scale);
+    });
+  });
+};
 
 const init = () => {
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
   getSizes(canvas, size4x4, state);
+  drawCanvas(size4x4, canvas, ctx);
 };
 
 init();
