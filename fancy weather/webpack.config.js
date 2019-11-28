c97a031756d1faf37f90d1cbbeb90dc9abbf499d
IrochkaZ <zaletk.irina@gmail.com>
2019-11-28 20:39:50 +0300

    fix: change webpack setup

--- fancy weather/webpack.config.js
+++ fancy weather/webpack.config.js
@@ -1,9 +1,10 @@
 const path  =  require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
 mode: 'production',
-entry: './src/index.js',
+entry: './src/js/index.js',
 output:{
   filename: 'main.js',
   path: path.resolve(__dirname,'dist')
@@ -12,7 +13,7 @@ output:{
     rules: [
       {
         test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
@@ -20,13 +21,30 @@ output:{
           }
         }
       },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap:true,
+          }
+        }
+        ]
+      }
     ],
   },
 plugins: [
   new HtmlWebpackPlugin({
-    title: 'fancy weather',
-    filename: 'fancy weather.html',
+    title: 'Fancy Weather App',
+    filename: 'index.html',
     template: './src/index.html'
   }),
-],
+  new MiniCssExtractPlugin({
+    filename: "./style.css",
+  })
+]
 }
