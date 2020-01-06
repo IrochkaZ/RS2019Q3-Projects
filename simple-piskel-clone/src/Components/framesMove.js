78fd64c8be2999b90f5420a50b24852a16f2202f
IrochkaZ <zaletk.irina@gmail.com>
2020-01-06 16:49:14 +0300

    feat: add function changeItemNumber

--- simple-piskel-clone/src/Components/framesMove.js
+++ simple-piskel-clone/src/Components/framesMove.js
@@ -1,6 +1,6 @@
-/* eslint-disable max-len */
-/* eslint-disable consistent-return */
-/* eslint-disable no-shadow */
+/* eslint-disable no-param-reassign */
+/* eslint-disable radix */
+
 import detete from '../assets/img/delete.png';
 import move from '../assets/img/drag.png';
 import duplicate from '../assets/img/duplicate.png';
@@ -13,6 +13,13 @@ const frameWrap = document.querySelector('.item-wrap');
 const framesMove = (ctx, obj) => {
   const { width, height } = obj.canvasSize;
   const addButtonFrame = document.querySelector('.add');
+  const changeItemNumber = () => {
+    const ulItems = document.getElementsByClassName('item__number');
+    Object.values(ulItems).forEach((el, index) => {
+      el.innerText = index + 1;
+    });
+  };
+
   addButtonFrame.addEventListener('click', () => {
     const frameItem = document.querySelectorAll('.frames .item');
     const item = frameItem[0].cloneNode(true);
@@ -23,9 +30,10 @@ const framesMove = (ctx, obj) => {
       el.classList.remove('active');
     });
     frameWrap.lastChild.classList.add('active');
+    changeItemNumber();
   });
 
-
+  // eslint-disable-next-line consistent-return
   const deleteFrame = (event) => {
     if (event.target.classList.contains('item__delete')) {
       if (frameWrap.children.length < 2) {
@@ -35,17 +43,28 @@ const framesMove = (ctx, obj) => {
         ctx.clearRect(0, 0, width, height);
       }
     }
+    changeItemNumber();
   };
-
 
   const duplicateFrame = (event) => {
     if (event.target.classList.contains('item__duplicate')) {
       const duplicateNode = event.target.parentNode.cloneNode(true);
       frameWrap.insertBefore(duplicateNode, event.target.parentNode);
+      document.querySelector('.item.active').classList.remove('active');
+      frameWrap.lastElementChild.classList.add('active');
+      changeItemNumber();
     }
   };
 
-  document.querySelector('.item-wrap').addEventListener('click', deleteFrame);
-  document.querySelector('.item-wrap').addEventListener('click', duplicateFrame);
+  document.querySelector('.item-wrap').addEventListener('click', (event) => {
+    if (event.target.classList.contains('item__duplicate')) {
+      duplicateFrame(event);
+    }
+
+    if (event.target.classList.contains('item__delete')) {
+      deleteFrame(event);
+    }
+  });
 };
+
 export default framesMove;
